e20d175ebfaceee3223e220214075b979aea44f1
Tom Smith <tomossmith86@icloud.com>
2023-09-30 17:28:37 +0000

    added member details pages

--- snookerclub/members/views.py
+++ snookerclub/members/views.py
@@ -1,7 +1,25 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import Member
 
 
 def members(request):
+    club_members = Member.objects.all().values()
     template = loader.get_template('members.html')
+    context = {
+        'club_members': club_members,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def details(request, id):
+    club_member = Member.objects.get(id=id)
+    template = loader.get_template('member_details.html')
+    context = {
+        'club_member': club_member,
+    }
+    return HttpResponse(template.render(context, request))
+
+def main(request):
+    template = loader.get_template('main.html')
     return HttpResponse(template.render())
